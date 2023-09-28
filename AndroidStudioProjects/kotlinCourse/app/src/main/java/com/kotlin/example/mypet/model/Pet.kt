e15d599895372b60f060cf469788ea6707affae1
LinhLH7 <v.linhlh7@vinsmart.net.vn>
2023-09-28 13:03:46 +0700

    my pet test

---
+++ AndroidStudioProjects/kotlinCourse/app/src/main/java/com/kotlin/example/mypet/model/Pet.kt
@@ -0,0 +1,10 @@
+package com.kotlin.example.mypet.model
+
+data class Pet (
+    val id: Int,
+    val name: Int,
+    val yearOld : Int,
+    val bread: Int,
+    val petImage: Int,
+    val category: Int
+)
